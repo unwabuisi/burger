56a6830a1ee4f1a8c1f93a064fb91de75486b355
unwabuisi <unwabuisi@utexas.edu>
2020-02-28 17:23:09 -0600

    db set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Classic Burger");
+INSERT INTO burgers (burger_name) VALUES ("Buffalo Bill");
+INSERT INTO burgers (burger_name) VALUES ("Lady Marmalade");
