83b588558550cf3e1924da8d93eddb23ca2fe8e7
unwabuisi <unwabuisi@utexas.edu>
2020-03-27 18:15:56 -0500

    sequalize set up

--- models/burgers.js
+++ models/burgers.js
@@ -1,25 +1,34 @@
 // import ORM to create functions
-var orm = require("../config/orm.js");
+// var orm = require("../config/orm.js");
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays.
+//   create: function(cols, vals, cb) {
+//     orm.create("burgers", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
 
 
 // export database functions for controller (burgerController.js)
-module.exports = burger;
+// module.exports = burger;
+
+
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+		burger_name: DataTypes.STRING,
+		devoured: DataTypes.BOOLEAN
+	});
+	return  Burger;
+};
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require('path');
-
 var port = process.env.PORT || 3000;
 
 var app = express();
+
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
@@ -23,6 +25,9 @@ var routes = require("./controllers/burgerController.js");
 
 app.use("/", routes);
 
-app.listen(port,function(){
-    console.log("app listening on port: "+ port);
+
+db.sequelize.sync().then(function(){
+    app.listen(port,function(){
+        console.log("app listening on port: "+ port);
+    });
 });
