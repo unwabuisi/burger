2b8c7d3f426a332681630d9c5579f9a64c881c74
unwabuisi <unwabuisi@utexas.edu>
2020-03-27 18:51:54 -0500

    Revert "sequalize set up"
    This reverts commit 83b588558550cf3e1924da8d93eddb23ca2fe8e7.

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require('path');
+
 var port = process.env.PORT || 3000;
 
 var app = express();
-
-var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 
 // Parse application/x-www-form-urlencoded
-// var bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
@@ -25,9 +23,6 @@ var routes = require("./controllers/burgerController.js");
 
 app.use("/", routes);
 
-
-db.sequelize.sync().then(function(){
-    app.listen(port,function(){
-        console.log("app listening on port: "+ port);
-    });
+app.listen(port,function(){
+    console.log("app listening on port: "+ port);
 });
