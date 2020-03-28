32c1d3fb0e13abd69e8e9d5b40246482e979233a
unwabuisi <unwabuisi@utexas.edu>
2020-03-28 16:50:05 -0500

    deleted custom ORM files + added burger name validation: no null or empty values allowed

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -13,18 +13,24 @@ router.get("/", function(req,res){
         };
         res.render("index", hbsObject);
     });
+
 });
 
 router.post("/", function(req,res){
     db.Burger.create({
         burger_name: req.body.burger_name
     }).then(function(result){
+        // console.log(result);
         res.status(200).redirect("/");
+    }).catch(function(err){
+        if (err.errors[0].path == "noEmpytyName") {
+            console.log(`Error: ${err.errors[0].message}`);
+        }
+
     });
 });
 
 router.put("/:id", function(req,res){
-    var condition = "id = " + req.params.id;
     db.Burger.update({
         devoured:true
     },
@@ -37,5 +43,7 @@ router.put("/:id", function(req,res){
         res.status(200).end();
     });
 });
+
+
 
 module.exports = router;
--- models/burgers.js
+++ models/burgers.js
@@ -2,11 +2,20 @@
 module.exports = function (sequelize, DataTypes){
     var Burger = sequelize.define("Burger", {
         burger_name: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            allowNull:false
             },
         devoured: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
+        }
+    }, {
+        validate:{
+            noEmpytyName() {
+                if (this.burger_name.length < 3) {
+                    throw new Error("Burger name must be at least 3 characters");
+                }
+            }
         }
     });
 return Burger;
