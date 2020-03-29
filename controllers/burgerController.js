03c8808bf66e07984b200703516c976bef18c6a8
unwabuisi <unwabuisi@utexas.edu>
2020-03-29 16:11:15 -0500

    fixed validation bug that was not allowing field to be updated correctly + added some new styling

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -20,27 +20,28 @@ router.post("/", function(req,res){
     db.Burger.create({
         burger_name: req.body.burger_name
     }).then(function(result){
-        // console.log(result);
+        console.log(result.dataValues);
         res.status(200).redirect("/");
     }).catch(function(err){
-        if (err.errors[0].path == "noEmpytyName") {
+        if (err.errors[0].path == "noEmpytyName" || err.errors[0].path == "burger_name") {
             res.status(500).send(err.errors[0]);
-            // console.log(`Error: ${err.errors[0].message}`);
+            console.log(`Error: ${err.errors[0].message}`);
         }
-
     });
 });
 
 router.put("/:id", function(req,res){
+
     db.Burger.update({
-        devoured:true
+        burger_name: req.body.name,
+        devoured:1
     },
     {
         where: {
-            id: req.params.id
+            id: req.body.id
         }
-    }
-    ).then(function(result){
+    }).then(function(result){
+
         res.status(200).end();
     });
 });
--- models/burgers.js
+++ models/burgers.js
@@ -3,7 +3,8 @@ module.exports = function (sequelize, DataTypes){
     var Burger = sequelize.define("Burger", {
         burger_name: {
             type: DataTypes.STRING,
-            allowNull:false
+            allowNull:false,
+            unique:true
             },
         devoured: {
             type: DataTypes.BOOLEAN,
